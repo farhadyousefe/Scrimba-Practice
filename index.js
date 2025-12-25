b6a7e4e20a08329c59868da07279a3de2d473011
Farhad Yousefe <farhadyousefe@gmail.com>
2025-12-25 10:39:32 +0430

    working with variables;

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+// Create two variables, firstName and lastName
+let firstName = "Farhad ";
+let lastName = "Yousefe";
 
-let welcomeEl = document.getElementById("welcome-el");
+// Concatenate the two variables into a third variable called fullName
 
-let name = "Farhad Yousefe";
-let greeting = "Welcome back ";
+let fullName = firstName + lastName;
 
-welcomeEl.innerText = greeting + name;
+// Log fullName to the console
 
-// Add an emoji to the end! 👋
-// WRITE YOUR CODE BELOW HERE
-// HINT: count = count + 1
+console.log("My name is " + fullName);
 
-welcomeEl.innerText += " 👋";
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greet() {
+  console.log(greeting + ", " + name);
+}
+
+greet();
+greet();
+
+let myPoints = 3;
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+
+function add3Points() {
+  myPoints += 3;
+}
+
+function remove1Point() {
+  myPoints -= 1;
+}
+
+// Call the functions to that the line below logs out 10
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+console.log(myPoints);
+
+console.log("2" + 2); // 22
+console.log(11 + 7); // 18
+console.log(6 + "5"); // 65
+console.log("My points: " + 5 + 9); // My points: 59
+console.log(2 + 2); //  4
+console.log("11" + "14"); //  1114
